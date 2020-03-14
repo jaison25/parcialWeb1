2f3cb1b40556e3ebdd74f9dce177a526411e5d0d
jaison25 <33075690+jaison25@users.noreply.github.com>
2020-03-13 19:49:36 -0500

    pandemia1

---
+++ pandemia1.js
@@ -0,0 +1,11 @@
+const pandemia1 = {
+    id : "",
+    Nombre : "H1N1",
+    Sintomas : "Diarrea",
+    Recomendaciones : "Usar Tapabocas",
+    Curso : "Falso",
+    Paises : "",
+    
+}
+
+module.exports.pandemia1 = pandemia1;
